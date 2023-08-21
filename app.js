930bcbafbab197527d450538151e7fc59639b62c
Ayushman88 <ayushmanmishra094@gmail.com>
2023-08-21 10:12:00 +0530

    Add id: element functionality in javascript

---
+++ app.js
@@ -0,0 +1,8 @@
+const typed = new Typed('#element', {
+    strings: ['HTML', 'CSS', 'JavaScript', 'React', 'Angular', 'Vue.js', 'Node.js', 'Express.js', 'Django', 'Flask',
+    'Python', 'Ruby', 'PHP', 'Java', 'C#', 'Go', 'Rust', 'Kotlin', 'Swift', 'Objective-C',
+    'Dart', 'Flutter', 'Firebase', 'AWS', 'Azure', 'Google Cloud', 'Heroku', 'Netlify', 'Vercel', 'DigitalOcean'    
+    ],
+    typeSpeed: 50,
+    loop: true
+});
